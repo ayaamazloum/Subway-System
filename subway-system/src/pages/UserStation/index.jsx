39f88a74cb9c05c074dafada48c40aaff3aea913
unknown <hanadi.hosri@gmail.com>
2024-03-30 22:00:13 +0200

    adding style

--- subway-system/src/pages/UserStation/index.jsx
+++ subway-system/src/pages/UserStation/index.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import "./style.css";
 
 const UserStation = () => {
 
     return (
-        <div><h1>Hello</h1></div>
+        <div className="page light-bg flex column">
+            <div className="input-container">
+                <FontAwesomeIcon icon={faSearch} className="search-icon" />
+                <input type="text" placeholder="Beirut" className="search-input"/>
+            </div>
+            <div>
+                <h2>Nearest Stations</h2>
+            </div>
+            <div>
+                <h2>Highest Rating Stations</h2>
+            </div>
+            <div>
+                <h2>All Stations</h2>
+            </div>
+        </div>
     )
 }
 
