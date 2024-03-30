7e134111632471bd37b33e7cf8aa34e366714ccf
unknown <hanadi.hosri@gmail.com>
2024-03-31 02:11:01 +0300

    adding small gap

--- subway-system/src/pages/UserStation/components/StationCard.jsx
+++ subway-system/src/pages/UserStation/components/StationCard.jsx
@@ -5,19 +5,19 @@ import { faLocationDot, faStar, faClock, faRestroom, faWheelchair, faWifi } from
 const StationCard = () => {
     
     return (
-    <div className="station-card white-bg semi-rounded gap" >
-        <div className="flex space-between">
+    <div className="station-card white-bg semi-rounded flex column gap" >
+        <div className="flex space-between gap">
             <p>Station 1</p>
-            <div className="flex gap">
+            <div className="flex small-gap">
                 <FontAwesomeIcon icon={faLocationDot} className="primary-text"/>
                 <p>Beirut</p>
             </div>
-            <div className="flex gap">
+            <div className="flex">
                 <FontAwesomeIcon icon={faStar} className="primary-text"/>
                 <p>4</p>
             </div>
         </div>
-        <div className="flex gap">
+        <div className="flex small-gap">
             <FontAwesomeIcon icon={faClock} className="primary-text"/>
             <p>3:00 am - 12:00 pm</p>
         </div>
