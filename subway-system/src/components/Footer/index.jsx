23eb07e1a897f9d6fa7d71c016b1f2d965f94a64
ayaamazloum <ayamazloum.a@gmail.com>
2024-03-31 04:56:56 +0300

    Add download section

---
+++ subway-system/src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import './style.css'
+
+const Footer = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Footer
--- subway-system/src/pages/Home/index.jsx
+++ subway-system/src/pages/Home/index.jsx
@@ -1,6 +1,12 @@
 import './style.css';
-import NavBar from "../Navbar";
+import NavBar from "../../components/Navbar";
 import HeroImage from "../../assets/images/hero-img.svg";
+import GooglePlay from "../../assets/icons/googleplay.svg";
+import AppStore from "../../assets/icons/appstore.svg";
+import DownloadBg from "../../assets/images/download-bg.svg";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Footer from '../../components/Footer';
 
 const Home = () => {
     return <div className="home-page white-bg full-width full-height">
@@ -36,6 +42,23 @@ const Home = () => {
                 <p className='xsm-text light-text'>Experienced Staff Members</p>
             </div>
         </div>
+
+        <div className='download-section flex column center gap-40 primary-bg rounded'>
+            <img className='download-bg' src={DownloadBg} />
+            <h1 className='large-text white-text'>Let us deliver you to your destination.</h1>
+            <div className="download-buttons flex row gap-70">
+                <button className='download-btn flex row center gap-10 white-bg bold secondary-text xsm-text semi-rounded'>
+                    <img className='download-icon' src={GooglePlay} />
+                    Google Play
+                </button>
+                <button className='download-btn flex row center gap-10 white-bg bold secondary-text xsm-text semi-rounded'>
+                    <img className='download-icon' src={AppStore} />
+                    App Sore
+                </button>
+            </div>
+        </div>
+
+        <Footer/>
     </div>;
 }
 
