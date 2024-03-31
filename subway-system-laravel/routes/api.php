7713fd4539846a595f472659abe953e15ce267ff
Ali Mansour <alialhadimansour21@gmail.com>
2024-04-01 00:58:05 +0300

    Implemented the Message api

--- subway-system-laravel/routes/api.php
+++ subway-system-laravel/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Branch\BranchMessageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('messages', [BranchMessageController::class, 'index']);
+Route::post('messages', [BranchMessageController::class, 'store']);
+Route::delete('messages/{id}', [BranchMessageController::class, 'destroy']);
