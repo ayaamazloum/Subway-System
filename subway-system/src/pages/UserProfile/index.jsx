b94602078e61b7e56e96b010c2766c2955b3b19c
unknown <hanadi.hosri@gmail.com>
2024-03-31 10:18:00 +0300

    adding and styling message container

--- subway-system/src/pages/UserProfile/index.jsx
+++ subway-system/src/pages/UserProfile/index.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMessage } from "@fortawesome/free-solid-svg-icons";
+import "./style.css";
 
 const UserProfile = () => {
 
 
     return (
-        <div><h1>hello</h1></div>
+        <div className="page light-bg flex column">
+            <div className="flex center">
+                <div className="input-container white-bg semi-rounded flex">
+                    <FontAwesomeIcon icon={faMessage} className="primary-text margin-left" />
+                    <input type="text" placeholder="Have anything to tell us ?" className="message-input" />
+                </div>
+                <div className="button-div">
+                    <button>Send</button>
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
