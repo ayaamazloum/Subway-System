1a663f2f4f7f8d6d5f47a654f3b91224e7bf9793
ayaamazloum <ayamazloum.a@gmail.com>
2024-04-03 02:12:05 +0300

    Add add a message by the passenger api

--- subway-system-laravel/app/Http/Controllers/PassengerMessageController.php
+++ subway-system-laravel/app/Http/Controllers/PassengerMessageController.php
@@ -13,13 +13,21 @@ class PassengerMessageController extends Controller
             ->join('branches', 'messages.receiver_id', '=', 'branches.user_id')
             ->join('stations', 'branches.id', '=', 'stations.branch_id')
             ->where('messages.sender_id', '=', auth()->id())
-            ->select('messages.message', 'messages.content', 'stations.name as station_name')
+            ->select('messages.content', 'stations.name as station_name')
             ->distinct()
             ->get();
         return response()->json(['status' => 'success', 'data' => $messages], 200);
     }
     public function store()
     {
-        
+        $data = request()->validate([
+            'receiver_id' => 'required',
+            'content' => 'required|string',
+        ]);
+        $data['sender_id'] = auth()->id();
+        $data['receiver_id'] = request()->receiver_id;
+        $data['content'] = request()->content;
+        Message::create($data);
+        return response()->json(['status' => 'success', 'messsage' => "Message Sent Successfully"], 200);
     }
 }
