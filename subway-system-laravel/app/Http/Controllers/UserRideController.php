132aacfd501cdaa09e832121520aa13edd326c86
unknown <hanadi.hosri@gmail.com>
2024-04-03 01:45:11 +0300

    implementing view passenger rides

--- subway-system-laravel/app/Http/Controllers/UserRideController.php
+++ subway-system-laravel/app/Http/Controllers/UserRideController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ride;
+use App\Models\Passenger;
 use Illuminate\Http\Request;
 
 class UserRideController extends Controller
@@ -17,5 +18,56 @@ class UserRideController extends Controller
             "message" => "success",
             "data" => $rides
         ], 200);
+    }
+
+    public function view_passenger_rides($passenger_id)
+    {
+        $passenger = Passenger::find($passenger_id);
+
+        if(!$passenger) {
+            return response()->json([
+                "message" => "passenger not found"
+            ], 404);
+        }
+
+        $rides = Ride::with('reviews')
+                    ->whereHas('tickets', function($query) use($passenger_id) {
+                        $query->where('passenger_id', $passenger_id);
+                    })
+                    ->get();
+
+        $formattedRides = $rides->map(function($ride) {
+            return [
+                'start_stations_latitude' => $ride->startStation->latitude,
+                'start_station_longitude' => $ride->startStation->longitude,
+                'end_station_latitude' => $ride->endStation->latitude,
+                'end_station_longitude' => $ride->endStation->longitude,
+                'start_time' => $ride->start_time,
+                'end_time' => $ride->end_time,
+                'capacity' => $ride->capacity,
+                'rating' => $this->calculateAverageRating($ride->reviews),
+            ];
+        });
+
+        return response()->json([
+            "message" => "success",
+            "data" => $formattedRides
+        ], 200);
+    }
+
+    private function calculateAverageRating($reviews)
+    {
+        $totalRating = 0;
+        $reviewCount = $reviews->count();
+
+        if ($reviewCount == 0) {
+            return null;
+        }
+
+        foreach ($reviews as $review) {
+            $totalRating += $review->rating;
+        }
+
+        return $totalRating / $reviewCount;
     }
 }
