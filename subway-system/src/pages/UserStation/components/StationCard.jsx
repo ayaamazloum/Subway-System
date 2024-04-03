7edca20be2100fd7f93591e564194b2ad7c59865
unknown <hanadi.hosri@gmail.com>
2024-04-03 07:28:01 +0300

    fetching location and making StationCard dynamic

--- subway-system/src/pages/UserStation/components/StationCard.jsx
+++ subway-system/src/pages/UserStation/components/StationCard.jsx
@@ -1,25 +1,46 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot, faStar, faClock, faRestroom, faWheelchair, faWifi } from "@fortawesome/free-solid-svg-icons";
 
-const StationCard = () => {
+const StationCard = ( {station} ) => {
     
+    const [locationName, setLocationName] = useState('');
+
+    useEffect(() => {
+        handleConvert();
+    }, []);
+    
+    const handleConvert = async () => {
+        try {
+            const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${station.latitude}&lon=${station.longitude}&format=json`);
+            if (response.data.display_name) {
+                setLocationName(response.data.display_name);
+            } else {
+                setLocationName('Location not found');
+            }
+            console.log(locationName);
+        } catch (error) {
+            console.error('Error fetching location:', error);
+            setLocationName('Error fetching location');
+        }
+    }
     return (
     <div className="station-card white-bg semi-rounded flex column gap" >
         <div className="flex space-between gap">
-            <p>Station 1</p>
+            <p>{station.name}</p>
             <div className="flex small-gap">
                 <FontAwesomeIcon icon={faLocationDot} className="primary-text"/>
-                <p>Beirut</p>
+                <p>{locationName}</p>
             </div>
             <div className="flex">
                 <FontAwesomeIcon icon={faStar} className="primary-text"/>
-                <p>4</p>
+                <p>{station.average_rating}</p>
             </div>
         </div>
         <div className="flex small-gap">
             <FontAwesomeIcon icon={faClock} className="primary-text"/>
-            <p>3:00 am - 12:00 pm</p>
+            <p>{station.operating_hours}</p>
         </div>
         <div className="flex center button-div">
             <button>View Rides</button>
