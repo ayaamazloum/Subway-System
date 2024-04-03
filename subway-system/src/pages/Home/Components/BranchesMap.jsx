d56fc82a96d9b33a2db4dbd690c7a152e947506a
ayaamazloum <ayamazloum.a@gmail.com>
2024-04-03 21:22:01 +0300

    Added dynamic popup for each station on map / Add map stations filter

--- subway-system/src/pages/Home/Components/BranchesMap.jsx
+++ subway-system/src/pages/Home/Components/BranchesMap.jsx
@@ -4,10 +4,12 @@ import { Icon } from "leaflet";
 import sendRequest from '../../../core/tools/remote/request';
 import { requestMehods } from "../../../core/enums/requestMethods";
 import markIcon from "../../../assets/icons/marker.png";
+import { useMapEvents } from "react-leaflet";
 
 function BranchesMap() {
-  const [stations, setStaions] = useState();
-  const [activeStation, setActiveStation] = useState(null);
+  const [allStations, setAllStaions] = useState();
+  const [stations, setStations] = useState();
+  const [searchText, setSearchText] = useState();
 
   const markerIcon = new Icon({
     iconUrl: markIcon,
@@ -19,7 +21,8 @@ function BranchesMap() {
       const res = await sendRequest(requestMehods.GET, "/view_stations", {});
       
       if (res.data.message === "success") {
-        setStaions(res.data.data);
+        setAllStaions(res.data.data);
+        setStations(res.data.data);
       }
     } catch (e) {
       console.error(e);
@@ -27,31 +30,39 @@ function BranchesMap() {
   }
 
   useEffect(() => {
+    const filteredStations = allStations?.filter((station) => station.name.includes(searchText));
+    setStations(filteredStations);
+  }, [searchText]);
+
+  useEffect(() => {
     loadStaions();
     console.log(stations);
   }, []);
+
   return (
     <div className="map-section section flex column center gap-40">
       <h1 className='secondary-text'>BROWSE OUR <span className='primary-text'>BRANCHES</span></h1>
-      <MapContainer center={[33.8547, 35.8623]} zoom={12} scrollWheelZoom={true}>
+      <input onChange={(e) => setSearchText(e.target.value)} className="map-search semi-rounded"
+        placeholder="Find a station" />
+      <MapContainer center={[33.8547, 35.8623]} zoom={11} scrollWheelZoom={true}>
         <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
-        {stations?.map(station => (
-          <Marker
-            key={station.id}
-            position={[
-              station.latitude,
-              station.longitude
-            ]}
-            onClick={() => {
-              setActiveStation(station);
-            }}
-            icon={markerIcon}
-          />
-        ))}
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
         
-        
+          {stations !== null && (
+          stations?.map(station => (
+              <Marker
+                key={station.id}
+                position={ [station.latitude, station.longitude] }
+                icon={markerIcon}>
+                <Popup>
+                    <h2>{station.name}</h2>
+                    <p>{station.operating_hours}</p>
+                    <p>{station.facilities}</p>
+                </Popup>
+              </Marker>
+          ))
+          )}
       </MapContainer>
     </div>
   );
